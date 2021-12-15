e6d7ede64fcac59a07aed83a0c7bde0ea0a15601
dp-depalobos <dindopaolo.depalobos@gmail.com>
2021-12-15 21:19:59 +0800

    * Updated code to be able to run in local unix instead of databricks, and very minor clean-Updated - Currently reworking logic to not depend on column names in making analysis

--- beginner.py
+++ beginner.py
@@ -1,8 +1,21 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, collect_list, struct
 
-spark = SparkSession.builder.appName('Morrisons').getOrCreate()
-df = spark.read.csv("dbfs:/FileStore/tables/morrisons/data.csv", header="true")
+def initializeSpark():
+    return SparkSession.builder \
+        .master("local") \
+        .config("spark.sql.autoBroadcastJoinThreshold", -1) \
+        .config("spark.executor.memory", "500mb") \
+        .appName("morisson") \
+        .getOrCreate()
+
+def formatToRaw(df):
+    return df.withColumn('concat', (struct(col('label'), col('ID'), col('link'))))
+
+spark = initializeSpark()
+
+input_path = "/home/dindo/client/data.csv"
+df = spark.read.csv(input_path, header="true")
 
 first_parent = df.select('Level 1 - Name', 'Level 1 - ID', 'Level 1 - URL') \
     .filter(df['Level 1 - ID'].isNotNull() & df['Level 2 - ID'].isNull()) \
@@ -24,21 +37,23 @@ third_parent = df.select('Level 2 - ID', 'Level 3 - Name', 'Level 3 - ID', 'Leve
     .withColumnRenamed('Level 3 - ID', 'ID') \
     .withColumnRenamed('Level 3 URL', 'link')
 
-third_df_raw = third_parent.withColumn('concat', (struct(col('label'), col('ID'), col('link'))))
+third_df_raw = formatToRaw(third_parent)
 third_df_agg = third_df_raw.groupBy(third_df_raw.pid).agg(collect_list(third_df_raw.concat))
 
 third_df_temp = third_df_agg.withColumnRenamed('collect_list(concat)', 'children').withColumnRenamed('pid', 'tempID')
-second_df_raw = second_parent.withColumn('concat', (struct(col('label'), col('ID'), col('link'))))
+second_df_raw = formatToRaw(second_parent)
 second_df_merged = second_df_raw.join(third_df_temp, second_df_raw.ID == third_df_temp.tempID, 'left')
 
 second_df_formatted = second_df_merged.withColumn('concat', (struct(col('label'), col('ID'), col('link'), col('children'))))
 second_df_agg = second_df_formatted.groupBy(second_df_formatted.pid).agg(collect_list(second_df_formatted.concat))
 
 second_df_temp = second_df_agg.withColumnRenamed('collect_list(concat)', 'children').withColumnRenamed('pid', 'tempID')
-first_df_raw = first_parent.withColumn('concat', (struct(col('label'), col('ID'), col('link'))))
+first_df_raw = formatToRaw(first_parent)
 first_df_merged = first_df_raw.join(second_df_temp, first_df_raw.ID == second_df_temp.tempID, 'left')
 
 first_df_formatted = first_df_merged.withColumn('concat', (struct(col('label'), col('ID'), col('link'), col('children'))))
 
 res = first_df_formatted.select('label', 'ID', 'link', 'children')
-res.write.json("dbfs:/FileStore/tables/morrisons/result/data.json")
+
+output_path = "/home/dindo/client/data.json"
+res.write.json(output_path)
