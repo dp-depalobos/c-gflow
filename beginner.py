5322d3b63a616e389c2d415145c5af8670c22619
dp-depalobos <dindopaolo.depalobos@gmail.com>
2021-12-22 14:52:34 +0800

    * added test cases for scenarios:     1. empty rows with column names     2. level one data     3. level two data     4. level three data     5. level four data
    * fixed issue in handling level one data

--- beginner.py
+++ beginner.py
@@ -1,22 +1,23 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, collect_list, lit, struct, udf
+from pyspark.sql.functions import array, col, collect_list, struct, udf
 from pyspark.sql.types import ArrayType, IntegerType, StringType, StructField, StructType
 
 def initializeSpark():
-    return SparkSession.builder \
-        .master("local") \
-        .config("spark.sql.autoBroadcastJoinThreshold", -1) \
-        .config("spark.executor.memory", "500mb") \
-        .appName("morisson") \
+    return SparkSession.builder\
+        .master("local")\
+        .config("spark.sql.autoBroadcastJoinThreshold", -1)\
+        .config("spark.executor.memory", "500mb")\
+        .appName("morisson")\
         .getOrCreate()
 
 def concat_columns(df, is_last_node=False):
     if is_last_node:
-        df = df.withColumn( \
-            'children', lit(None).cast(ArrayType(StructType())))
-    return df.withColumn( \
-        'concat', (struct( \
-            col('label'), col('ID'), \
+        df = df.withColumn(\
+            'children',  array().cast(ArrayType(StringType())))
+    df = df.na.fill("[]", ["children"])
+    return df.withColumn(\
+        'concat', (struct(\
+            col('label'), col('ID'),\
             col('link'), col('children'))))
 
 def count_not_null(*args):
@@ -26,33 +27,34 @@ def count_not_null(*args):
             i += 1
     return i
 
-def add_count_column(df):
+def add_depth_column(df):
     my_udf = udf(lambda *x: count_not_null(*x), IntegerType())
     df_columns = [col(c) for c in df.columns if 'ID' in c]
-    return df.withColumn("count", my_udf(*df_columns))
+    return df.withColumn('depth', my_udf(*df_columns))
 
 def get_first_node(df, name, id, url, depth):
-    return df.select(name, id, url) \
-        .filter(df['count'] == depth) \
-        .withColumnRenamed(name, 'label') \
-        .withColumnRenamed(id, 'ID') \
+    return df.select(name, id, url)\
+        .filter(df['depth'] == depth)\
+        .withColumnRenamed(name, 'label')\
+        .withColumnRenamed(id, 'ID')\
         .withColumnRenamed(url, 'link')
 
 def get_subsequent_node(df, parent, name, id, url, depth):
-    return df.select(parent, name, id, url) \
-        .filter(df['count'] == depth) \
-        .withColumnRenamed(parent, 'pid') \
-        .withColumnRenamed(name, 'label') \
-        .withColumnRenamed(id, 'ID') \
-        .withColumnRenamed(url, 'link') \
+    return df.select(parent, name, id, url)\
+        .filter(df['depth'] == depth)\
+        .withColumnRenamed(parent, 'pid')\
+        .withColumnRenamed(name, 'label')\
+        .withColumnRenamed(id, 'ID')\
+        .withColumnRenamed(url, 'link')\
 
 def aggregate_child_node(df):
     df = df.groupBy(df.pid).agg(collect_list(df.concat))
-    return df.withColumnRenamed('collect_list(concat)', 'children') \
+    return df.withColumnRenamed('collect_list(concat)', 'children')\
         .withColumnRenamed('pid', 'tempID')
 
 def get_column_names(df, depth):
-    columns = [c for c in df.columns if str(depth) in c]
+    pattern = str(depth) + str(' ')
+    columns = [c for c in df.columns if pattern in c]
     name = ''
     url = ''
     id = ''
@@ -65,56 +67,69 @@ def get_column_names(df, depth):
             id = c
     return name, id, url
 
-def get_max_count(df):
-    return df.agg({'count': 'max'}).collect()[0][0]
+def get_max_depth(df):
+    return df.agg({'depth': 'max'}).collect()[0][0]
 
-def get_depth_node_map(df, max_count):
-    depth_node_map = {}
-    for i in range(max_count, 0, -1):
+def get_depth_node_dict(df, max_depth):
+    depth_node_dict = {}
+    for i in range(max_depth, 0, -1):
         if i == 1:
             columns = get_column_names(df, i)
-            depth_node_map[i] = get_first_node(\
+            depth_node_dict[i] = get_first_node(\
                 df, columns[0], columns[1], columns[2], i)
         else:
             columns = get_column_names(df, i)
             parent_id = 'Level ' + str(i-1) + str(' - ID')
-            depth_node_map[i] = get_subsequent_node(\
+            depth_node_dict[i] = get_subsequent_node(\
                 df, parent_id, columns[0], columns[1], columns[2], i)
-    return depth_node_map
+    return depth_node_dict
 
-spark = initializeSpark()
+def combine_nodes(depth_node_dict, max_depth):
+    agg = {}
+    if max_depth == 1:
+        formatted = concat_columns(depth_node_dict[1], True)
+        agg[1] = formatted
+        return agg
 
-input_path = "/home/dindo/client/data3.csv"
-df = spark.read.csv(input_path, header="true")
-df = add_count_column(df)
+    for i in range(max_depth, 0, -1):
+        if max_depth == i:
+            formatted = concat_columns(depth_node_dict[i], True)
+            agg[i] = aggregate_child_node(formatted)
+        elif i == 1:
+            agg[i] = depth_node_dict[i].join(\
+                agg[i+1], depth_node_dict[i].ID == agg[i+1].tempID, 'left')
+        else:
+            merged = depth_node_dict[i].join(\
+                agg[i+1], depth_node_dict[i].ID == agg[i+1].tempID, 'left')
+            formatted = concat_columns(merged)
+            agg[i] = aggregate_child_node(formatted)
+    return agg
 
-max_count = get_max_count(df)
-depth_node_map = get_depth_node_map(df, max_count)
-
-agg = {}
-for i in range(max_count, 0, -1):
-    if max_count == i:
-        formatted = concat_columns(depth_node_map[i], True)
-        agg[i] = aggregate_child_node(formatted)
-    elif i == 1:
-        agg[i] = depth_node_map[i].join( \
-            agg[i+1], depth_node_map[i].ID == agg[i+1].tempID, 'left')
+def create_result(agg, spark):
+    if agg:
+        result = agg[1].select('label', 'ID', 'link', 'children')
     else:
-        merged = depth_node_map[i].join( \
-            agg[i+1], depth_node_map[i].ID == agg[i+1].tempID, 'left')
-        formatted = concat_columns(merged)
-        agg[i] = aggregate_child_node(formatted)
+        schema = StructType([\
+            StructField('label', StringType(), True),\
+            StructField('ID', StringType(), True),\
+            StructField('link', StringType(), True),\
+            StructField('children', ArrayType(StructType()), True)])
+        result = spark.createDataFrame(spark.sparkContext.emptyRDD(), schema)
+    return result
 
-schema = StructType([ \
-    StructField('label', StringType(), True), \
-    StructField('ID', StringType(), True), \
-    StructField('link', StringType(), True), \
-    StructField('children', ArrayType(StructType()), True)])
+def handle(path, spark):
+    df = spark.read.csv(path, header="true")
+    df = add_depth_column(df)
 
-if agg:
-    res = agg[1].select('label', 'ID', 'link', 'children')
-else:
-    res = spark.createDataFrame(spark.sparkContext.emptyRDD(), schema)
+    max_depth = get_max_depth(df)
+    depth_node_dict = get_depth_node_dict(df, max_depth)
+    agg = combine_nodes(depth_node_dict, max_depth)
+    return create_result(agg, spark)
 
-output_path = "/home/dindo/client/data.json"
-res.write.json(output_path, 'overwrite')
+def write_output(result, path = "/home/dindo/client/data.json"):
+    result.write.json(path=path, ignoreNullFields=False, mode='overwrite')
+
+# path = "/home/dindo/t2/c-gflow/test_csvs/original_data.csv"
+# spark = initializeSpark()
+# res = handle(path, spark)
+# write_output(res)
