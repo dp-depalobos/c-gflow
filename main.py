7406fe310b83b6ac4c8401d20111b0696f0b904f
dp-depalobos <dindopaolo.depalobos@gmail.com>
2022-01-06 20:45:47 +0800

    cleanup

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import csv
-import os
-import io
-import copy
-
 from src.app import app
 from src.csv_operator import CsvOperator as csv_opr
 from flask import flash, request, redirect, render_template
-from werkzeug.utils import secure_filename
 
 from src.handler import handle
 ALLOWED_EXTENSION = "csv"
@@ -22,31 +16,28 @@ def upload_file():
         if "file" not in request.files:
             flash("No file part")
             return redirect(request.url)
+
         file = request.files["file"]
         if file.filename == "":
             flash("No file selected for uploading")
             return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-            file.save(filepath)
-            c_opr = csv_opr(filepath)
-            if not c_opr.validate_len():
-                flash("Empty file/rows")
-                os.remove(filepath)
-                return redirect(request.url)
-            if not c_opr.validate_header():
-                flash("Missing mandatory columns")
-                os.remove(filepath)
-                return redirect(request.url)
-            else:
-                result = handle(filepath)
-                os.remove(filepath)
-                flash("File successfully uploaded")
-                return render_template("upload.html", filename=result)
-        else:
+
+        if not allowed_file(file.filename):
             flash("Allowed file type: CSV")
             return redirect(request.url)
+
+        c_opr = csv_opr(file.read())
+        if not c_opr.validate_len():
+            flash("Empty file/rows")
+            return redirect(request.url)
+
+        if not (c_opr.valid_header and c_opr.depth > 0):
+            flash("Missing mandatory columns")
+            return redirect(request.url)
+
+        result = handle(c_opr.columns, c_opr.rows, c_opr.depth)
+        flash("File successfully uploaded")
+        return render_template("upload.html", filename=result)
 
 @app.route("/")
 def upload_form():
